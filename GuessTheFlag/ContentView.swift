00f9b1a6050bf04be1a95793ad886521db1dcad2
Katios <kate.chonkina@gmail.com>
2020-12-30 12:02:45 +0600

    Challenge

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var userScore = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
@@ -40,10 +41,11 @@ struct ContentView: View {
                     }
                 }
                 Spacer()
-                
+                Text("Your current score is \(userScore)")
+                    .foregroundColor(.white).font(.largeTitle)
             }
             .alert(isPresented: $showingScore) {
-                Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                Alert(title: Text(scoreTitle), message: Text("Your score is \(userScore)"), dismissButton: .default(Text("Continue")) {
                     self.askQuestion()
                 })
             }
@@ -52,9 +54,10 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         }
         else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's flag of \(self.countries[number])"
         }
         showingScore = true
     }
